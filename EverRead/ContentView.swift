21ed625ef655f31315bf4076b1e2a2bc101dc009
Worawalan C <113490970+woraamy@users.noreply.github.com>
2025-04-15 20:16:38 +0700

    Initial Commit

---
+++ EverRead/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EverRead
+//
+//  Created by Worawalan Chatlatanagulchai on 15/4/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EverRead/EverReadApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverReadApp.swift
+//  EverRead
+//
+//  Created by Worawalan Chatlatanagulchai on 15/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct EverReadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
