44ef4459dec3e27dc02648bf0e79c98cb5593ef0
tnnpp <tonnam.napasorn@gmail.com>
2025-04-24 00:27:28 +0800

    add set goal dialog

--- EverRead/Components/ReadGoalCard.swift
+++ EverRead/Components/ReadGoalCard.swift
@@ -1,60 +1,131 @@
 import SwiftUI
 
 struct ReadGoalCard: View {
-    var yearGoalValue:Double = 0.29
-    var monthGoalValue:Double = 0.5
+    var yearGoalValue:Int = 29
+    var monthGoalValue:Int = 50
+    @State var yearGoalTotal:Int = 0
+    @State var monthGoalTotal:Int = 0
+    @State var showYearlyDialog:Bool = false
+    @State var showMonthlyDialog:Bool = false
     var body: some View {
         VStack(alignment: .leading){
             Text("Reading Goals").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
             HStack{
                 // Yearly Goal
-                VStack(alignment: .leading){
-                    HStack{
-                        Image(systemName: "calendar")
-                        Text("Yearly Goal")
-                    }
-                    Text("7").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
-                    HStack{
-                        Text("Of 24 books").font(.caption)
-                        Spacer()
-                        Text("29%").font(.subheadline).bold()
-                    }
-                    ZStack(alignment: .leading) {
-                        Capsule()
-                            .frame(height: 10)
-                            .foregroundColor(.gray.opacity(0.2))
-                                Capsule()
-                                    .frame(width: CGFloat(yearGoalValue) * 150 , height: 10)
-                                    .foregroundColor(.pink)
-                            }
-                }.padding().frame(width: 175,alignment: .leading).background(Color.redPink).clipShape(RoundedRectangle(cornerRadius: 10))
+                Button(action: {
+                    showYearlyDialog = true
+                }){
+                    VStack(alignment: .leading){
+                        HStack{
+                            Image(systemName: "calendar")
+                            Text("Yearly Goal")
+                        }
+                        Text("7").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
+                        HStack{
+                            Text("Of \(yearGoalTotal) books").font(.caption)
+                            Spacer()
+                            Text("\(yearGoalValue) %").font(.subheadline).bold()
+                        }
+                        ZStack(alignment: .leading) {
+                            Capsule()
+                                .frame(height: 10)
+                                .foregroundColor(.gray.opacity(0.2))
+                            Capsule()
+                                .frame(width: CGFloat(yearGoalValue) * 150 * 0.01, height: 10)
+                                .foregroundColor(.pink)
+                        }
+                    }.padding().frame(width: 175,alignment: .leading).background(Color.redPink).clipShape(RoundedRectangle(cornerRadius: 10))
+                }.foregroundColor(.black)
                 //Monthly Goal
-                VStack(alignment: .leading){
-                    HStack{
-                        Image(systemName: "book.circle")
-                        Text("Monthly Goal")
-                    }
-                    Text("1").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
-                    HStack{
-                        Text("Of 2 books").font(.caption)
-                        Spacer()
-                        Text("50%").font(.subheadline).bold()
-                    }
-                    ZStack(alignment: .leading) {
-                        Capsule()
-                            .frame(height: 10)
-                            .foregroundColor(.gray.opacity(0.2))
-                                Capsule()
-                                    .frame(width: CGFloat(monthGoalValue) * 150 , height: 10)
-                                    .foregroundColor(.pink)
-                            }
-                }.padding().frame(width: 175,alignment: .leading).background(Color.redPink).clipShape(RoundedRectangle(cornerRadius: 10))
-                                                                                                      }
-        }
+                Button(action: {
+                    showMonthlyDialog = true
+                }){
+                    VStack(alignment: .leading){
+                        HStack{
+                            Image(systemName: "book.circle")
+                            Text("Monthly Goal")
+                        }
+                        Text("1").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
+                        HStack{
+                            Text("Of \(monthGoalTotal) books").font(.caption)
+                            Spacer()
+                            Text("\(monthGoalValue)%").font(.subheadline).bold()
+                        }
+                        ZStack(alignment: .leading) {
+                            Capsule()
+                                .frame(height: 10)
+                                .foregroundColor(.gray.opacity(0.2))
+                            Capsule()
+                                .frame(width: CGFloat(monthGoalValue) * 150 * 0.01 , height: 10)
+                                .foregroundColor(.pink)
+                        }
+                    }.padding().frame(width: 175,alignment: .leading).background(Color.redPink).clipShape(RoundedRectangle(cornerRadius: 10))
+                }.foregroundColor(.black)
+            }.sheet(isPresented: $showYearlyDialog) {
+                SetYearlytGoalView(goal:$yearGoalTotal)
+            }.sheet(isPresented: $showMonthlyDialog) {
+                SetMonthlytGoalView(goal:$monthGoalTotal)
+            }        }
+    }
+}
+
+struct SetYearlytGoalView: View {
+    @Binding var goal: Int
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+            ZStack(){
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.pink.opacity(0.3), Color.white]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                ).ignoresSafeArea()
+                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                {
+                    Text("Set Yearly Goal")
+                            .font(.title)
+                            .bold().padding()
+                    Text("Set the number of books you want to read this year!")
+                    TextField("Books", value: $goal, format: .number)
+                        .textFieldStyle(RoundedBorderTextFieldStyle()).multilineTextAlignment(.center).padding().onSubmit {
+                            dismiss()
+                        }
+                        .frame(width: 200)
+                }
+                .padding()
+                
+            }
         
     }
 }
 
+struct SetMonthlytGoalView: View {
+    @Binding var goal: Int
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+            ZStack(){
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.pink.opacity(0.3), Color.white]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                ).ignoresSafeArea()
+                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                {
+                    Text("Set Monthly Goal")
+                            .font(.title)
+                            .bold().padding()
+                    Text("Set the number of books you want to read this month!")
+                    TextField("Books", value: $goal, format: .number)
+                        .textFieldStyle(RoundedBorderTextFieldStyle()).multilineTextAlignment(.center).padding().onSubmit {
+                            dismiss()
+                        }
+                        .frame(width: 200)
+                }
+                .padding()
+                
+            }
+        
+    }
+}
 #Preview {
     ReadGoalCard()
 }
